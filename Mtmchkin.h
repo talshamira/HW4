cd8555757e3d52dde8c7431505e89d4297029a3b
talshamira <talshamira@gmail.com>
2023-06-13 14:17:57 +0300

    added given files

---
+++ Mtmchkin.h
@@ -0,0 +1,53 @@
+#ifndef MTMCHKIN_H_
+#define MTMCHKIN_H_
+
+class Mtmchkin{
+
+public:
+    
+    /*
+    * C'tor of Mtmchkin class
+    *
+    * @param filename - a file which contains the cards of the deck.
+    * @return
+    *      A new instance of Mtmchkin.
+    */
+    explicit Mtmchkin(const std::string &fileName);
+    
+    /*
+    * Play the next Round of the game - according to the instruction in the exercise document.
+    *
+    * @return
+    *      void
+    */
+    void playRound();
+    
+    /*
+    * Prints the leaderBoard of the game at a given stage of the game - according to the instruction in the exercise document.
+    *
+    * @return
+    *      void
+    */
+    void printLeaderBoard() const;
+    
+    /*
+    *  Checks if the game ended:
+    *
+    *  @return
+    *          True if the game ended
+    *          False otherwise
+    */
+    bool isGameOver() const;
+    
+	/*
+    *  Returns the number of rounds played.
+    *
+    *  @return
+    *          int - number of rounds played
+    */
+    int getNumberOfRounds() const;
+};
+
+
+
+#endif /* MTMCHKIN_H_ */
